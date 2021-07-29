498eefe6794486bb96fa27d7a28f4ecb34fe7662
Atvik Singh <singhatvik@gmail.com>
2021-07-29 23:40:16 +0530

    Updated index page photos

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,12 @@
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 const Campground = require("../models/campground");
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const { descriptors, places } = require("./seedHelpers");
+
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
